e80b50a6dc8b58b8154f5291a4e160032f895120
Restu-Averian <restuaverianputra123@gmail.com>
2023-12-27 09:53:51 +0700

    make crsl component, updt relation to carousel

--- src/components/detail-anime/recommendation-anime/RecommendationAnime.jsx
+++ src/components/detail-anime/recommendation-anime/RecommendationAnime.jsx
@@ -1,72 +1,22 @@
-import { Heading, SimpleGrid, Stack, Text } from "@chakra-ui/react";
+import { Heading, Text } from "@chakra-ui/react";
 import CardData from "../../global/CardData";
-import { Carousel } from "react-responsive-carousel";
-import BgImage from "../../global/BgImage";
 import { useMemo } from "react";
-import { Link } from "react-router-dom";
 import formatWord from "../../../helpers/formatWord";
-import useResponsive from "../../../hooks/useResponsive";
+import CarouselAnime from "../../global/CarouselAnime";
 
 const RecommendationAnime = ({ data }) => {
-  const { sm } = useResponsive();
-
-  const dataPerSlide = useMemo(() => {
-    return sm ? 2 : 3;
-  }, [sm]);
-
-  /** Proses data untuk nampilin 3 or 2 anime per carousel
-   * 0 - 3 / 0 -2
-   * 3 - 6 / 2 - 4
-   */
-  const dataCarousel = useMemo(() => {
-    const tempData = [];
-    data?.recommendations?.forEach((_, idx) => {
-      tempData?.push(
-        data?.recommendations?.slice(
-          idx * dataPerSlide,
-          (idx + 1) * dataPerSlide
-        )
-      );
-    });
-
-    return tempData?.filter((d) => d?.length > 0);
-  }, [data, sm]);
+  const datasCarousel = useMemo(() => {
+    return data?.recommendations?.map((item) => ({
+      id: item?.id,
+      title: item?.title?.romaji,
+      subTitle: <Text>{formatWord(item?.type)}</Text>,
+      cover: item?.cover,
+    }));
+  }, [data]);
 
   return (
     <CardData useDefault header={<Heading>Recommendations</Heading>}>
-      <Carousel
-        emulateTouch
-        showIndicators={false}
-        infiniteLoop
-        swipeable
-        autoPlay
-        showStatus={false}
-      >
-        {dataCarousel?.map((recom) => {
-          return (
-            <SimpleGrid
-              key={recom?.id}
-              columns={dataPerSlide}
-              spacing={sm ? 5 : 10}
-            >
-              {recom?.map((r) => (
-                <Link key={r?.id} to={`/anime/${r?.id}/${r?.title?.romaji}`}>
-                  <BgImage src={r?.cover} useOverlay height={300}>
-                    <Stack spacing={2} bottom={5} left={2} pos="absolute">
-                      <Heading as="h3" fontSize={sm ? "md" : "xl"}>
-                        {r?.title?.romaji}
-                      </Heading>
-                      <Stack direction="row">
-                        <Text>{formatWord(r?.type)}</Text>
-                      </Stack>
-                    </Stack>
-                  </BgImage>
-                </Link>
-              ))}
-            </SimpleGrid>
-          );
-        })}
-      </Carousel>
+      <CarouselAnime arrDatas={datasCarousel} />
     </CardData>
   );
 };
--- src/components/detail-anime/relations-anime/RelationsAnime.jsx
+++ src/components/detail-anime/relations-anime/RelationsAnime.jsx
@@ -1,38 +1,28 @@
-import { Heading, SimpleGrid, Stack, Text } from "@chakra-ui/react";
+import { Heading, Text } from "@chakra-ui/react";
 import CardData from "../../global/CardData";
-import BgImage from "../../global/BgImage";
-import { Link } from "react-router-dom";
 import formatWord from "../../../helpers/formatWord";
-import useResponsive from "../../../hooks/useResponsive";
+import CarouselAnime from "../../global/CarouselAnime";
+import { useMemo } from "react";
 
 const RelationsAnime = ({ data }) => {
-  const { sm } = useResponsive();
+  const dataCarousel = useMemo(() => {
+    return data?.relations?.map((item) => ({
+      id: item?.id,
+      title: item?.title?.romaji,
+      subTitle: (
+        <>
+          <Text>{formatWord(item?.type)}</Text>
+          <Text>•</Text>
+          <Text>{formatWord(item?.relationType)}</Text>
+        </>
+      ),
+      cover: item?.image,
+    }));
+  }, [data]);
 
   return (
     <CardData useDefault header={<Heading>Relations</Heading>}>
-      <SimpleGrid columns={sm ? 2 : 3} spacing={5}>
-        {data?.relations?.map((relate) => {
-          return (
-            <Link
-              key={relate?.id}
-              to={`/anime/${relate?.id}/${relate?.title?.romaji}`}
-            >
-              <BgImage src={relate?.image} useOverlay cursor="pointer">
-                <Stack spacing={2} bottom={5} left={2} pos="absolute">
-                  <Heading as="h3" fontSize={sm ? 16 : "xl"}>
-                    {relate?.title?.romaji}
-                  </Heading>
-                  <Stack direction="row" fontSize={sm ? 12 : "xl"}>
-                    <Text>{formatWord(relate?.type)}</Text>
-                    <Text>•</Text>
-                    <Text>{formatWord(relate?.relationType)}</Text>
-                  </Stack>
-                </Stack>
-              </BgImage>
-            </Link>
-          );
-        })}
-      </SimpleGrid>
+      <CarouselAnime arrDatas={dataCarousel} />
     </CardData>
   );
 };
